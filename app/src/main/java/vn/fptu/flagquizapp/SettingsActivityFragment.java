572a331305422c582da26a69a968238fc0ded166
qnhanhh <caoquynhanh18@gmail.com>
2023-02-21 17:32:41 +0700

    Initial commit

---
+++ app/src/main/java/vn/fptu/flagquizapp/SettingsActivityFragment.java
@@ -0,0 +1,12 @@
+package vn.fptu.flagquizapp;
+
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+
+public class SettingsActivityFragment extends PreferenceFragment {
+    @Override
+    public void onCreate(Bundle bundle) {
+        super.onCreate(bundle);
+        addPreferencesFromResource(R.xml.preferences);
+    }
+}
